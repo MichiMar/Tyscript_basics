3039a186656152d33c21f31edd836afe3c2846d0
MichiMar <michimar.alexis@gmail.com>
2022-05-08 10:45:48 -0600

    string and number in alias

--- 009_union_types.js
+++ 009_union_types.js
@@ -1,8 +1,4 @@
 var playerA = ["Altuve", "Corerra", "Bregman"];
 var player_numbers = [2, 3, 4];
 console.log(playerA);
-console.log(player_numbers);
-
-var names: string[] | string;
-names = ["Jordan"]
-    //# sourceMappingURL=009_union_types.js.map
+//# sourceMappingURL=009_union_types.js.map
