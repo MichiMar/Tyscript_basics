dbb4e82efa630aed2f848edde9cabebc9378b7b2
MichiMar <michimar.alexis@gmail.com>
2022-05-11 21:16:08 -0600

    conditionals operators and godd and bad practices in ts conditionals

---
+++ 012_conditionals_operators.ts
@@ -0,0 +1,42 @@
+let x : number = 100;
+
+//Different options to use for math operators
+// if (x == 200) {
+//     console.log('Condition passed'); 
+// }
+
+//If ask for a string typescript will say it is not posible to compare a number with a str
+
+// if (x == '100') {
+//     console.log('Condition passed');
+    
+// }
+
+// //Triple iquals is used in js for developers to inforces the result, it only check for the equivalence of a value
+// // The conditional will not pass
+// if (x === '100') {
+//     console.log('Condition passed');
+    
+// }
+
+// // != will work as regular
+// if (x != 100) {
+//     console.log('Condition passed');
+    
+// }
+
+if (x > 100) {
+    console.log('Condition passed');
+}
+
+if (x >= 100) {
+    console.log('Condition passed');
+}
+
+if (x < 100) {
+    console.log('Condition passed');
+}
+
+if (x <= 100) {
+    console.log('Condition passed');
+}
