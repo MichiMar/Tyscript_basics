a83106836932c061c7545fc63fbdba59ae4680fb
MichiMar <michimar.alexis@gmail.com>
2022-05-11 21:30:49 -0600

    different  practices in conditionals

---
+++ 013_compuond_conditinals.js
@@ -0,0 +1,13 @@
+var email = 'test@tes.com';
+var password = 'asdfasdf';
+// // if (password == 'asdfasdf' && email == 'test@tes.com') {
+// if (password == 'asdfasdf' || password == 'zxcvzxcv') {
+//     console.log('Password is correct');
+// }  else {
+//     console.log('Permission denied');    
+// }
+// *This code is correct, but is not common to use it to get the exact different result
+if (!(email == 'tesst@test.com')) {
+    console.log('You are authotized');
+}
+//# sourceMappingURL=013_compuond_conditinals.js.map
