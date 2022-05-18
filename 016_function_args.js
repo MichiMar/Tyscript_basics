ee3c18e61c5614c684f5e01455dc02bb241e7b19
MichiMar <michimar.alexis@gmail.com>
2022-05-18 00:09:21 -0600

    updating arguments 2

--- 016_function_args.js
+++ 016_function_args.js
@@ -1,10 +1,14 @@
-//optional arguments
-function printAddress(street, streetTwo) {
+// Optional arguments
+function printAddress(street, streetTwo, state) {
+    if (state === void 0) { state = 'AZ'; }
     console.log(street);
     if (streetTwo) {
         console.log(streetTwo);
     }
+    console.log(state);
 }
 printAddress('Av siempreviva 123');
 printAddress('Av siempreviva 123', 'Suite 540');
+printAddress('Av siempreviva 123', 'Suite 540', 'UT');
+// Default arguments
 //# sourceMappingURL=016_function_args.js.map
--- 016_function_args.ts
+++ 016_function_args.ts
@@ -1,10 +1,15 @@
-//optional arguments
-function printAddress (street:string, streetTwo?:string) {
-    console.log(street);
-    if (streetTwo) {
-        console.log(streetTwo);
-    }
-}
+// // Optional arguments
+// function printAddress (street:string, streetTwo?:string, state = 'AZ') {
+//     console.log(street);
+//     if (streetTwo) {
+//         console.log(streetTwo);
+//     }
+//     console.log(state);
+    
+// }
 
-printAddress('Av siempreviva 123')
-printAddress('Av siempreviva 123', 'Suite 540')
+// printAddress('Av siempreviva 123')
+// printAddress('Av siempreviva 123', 'Suite 540')
+// printAddress('Av siempreviva 123', 'Suite 540', 'UT')
+
+// Default arguments
